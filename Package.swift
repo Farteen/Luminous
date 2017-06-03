f87c0f65f44f5968006f18bfd44e11b9aeedf988
farteen <xujavacn@gmail.com>
2017-06-03 08:49:38 +0800

    edit Package.swift exclude

--- Package.swift
+++ Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let package = Package(
 	name: "Luminous",
 	targets: [Target(name: "Luminous")],
-	exclude: ["Example/*", "README.md", "LICENSE", "Luminous.podspec"]
+	exclude: ["Example", "README.md", "LICENSE", "Luminous.podspec"]
 )
